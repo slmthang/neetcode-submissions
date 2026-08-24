2f9a6c5e5793ef1670f833616f70abaed9051afd
slmthang <163381046+slmthang@users.noreply.github.com>
2026-08-24 01:38:43 -0400

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,48 @@
+class Solution:
+    # def isPalindrome(self, s: str) -> bool:
+    #     i = 0
+    #     j = len(s) - 1
+
+    #     while (i < j):
+
+    #         while(i < j and not s[i].isalnum()):
+    #             i += 1
+            
+    #         while(i < j and not s[j].isalnum()):
+    #             j -= 1
+
+    #         if (i < j and s[i].lower() != s[j].lower()):
+    #             return False
+
+    #         i += 1
+    #         j -= 1
+        
+    #     return True
+
+    # custom alp()
+    def isPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s) - 1
+
+        while (i < j):
+
+            while(i < j and not self.alp(s[i])):
+                i += 1
+            
+            while(i < j and not self.alp(s[j])):
+                j -= 1
+
+            if (i < j and s[i].lower() != s[j].lower()):
+                return False
+
+            i += 1
+            j -= 1
+        
+        return True
+
+    def alp(self, c) -> bool:
+        return (
+            (ord(c) >= ord('a') and ord(c) <= ord('z')) or
+            (ord(c) >= ord('A') and ord(c) <= ord('Z')) or
+            (ord(c) >= ord('0') and ord(c) <= ord('9'))
+        )
